fe1c958679931fb05fa161e9363e9d30655a0c9c
Fabs <fsalaman@gmail.com>
2018-05-22 23:08:12 -0700

    Store complete, pending JSON filters: Fabian Salamanca

--- src/payclipcch/ListTransactions.java
+++ src/payclipcch/ListTransactions.java
@@ -10,5 +10,8 @@ package payclipcch;
 public class ListTransactions {
     public static void tlist(String user_id){
         System.out.println("List from User ID: "+user_id);
+        StorageBean leer = new StorageBean(0);
+        String salida = leer.readdata("pcchstore.dat");
+        System.out.println(salida);
     }
 }
--- src/payclipcch/StorageBean.java
+++ src/payclipcch/StorageBean.java
@@ -1,15 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package payclipcch;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.attribute.*;
+import java.util.List;
 import java.io.FileWriter;
 import java.io.IOException;
-import org.json.JSONObject;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+//import org.json.JSONObject;
+import java.nio.file.Paths;
 
 /**
  *
@@ -36,46 +40,80 @@ public class StorageBean {
     }
     
     private void saveFile(String transaction) {
-    BufferedWriter writer = null;
+    	BufferedWriter writer = null;
+    	String pafile = "pcchstore.dat";
+    	
         try {
             //create text store
-            File logFile = new File("payclipstore.dat");
+        	if(!checkife(pafile, transaction)) {
+        		File logFile = new File(pafile);
+            
+        		// Check real path
+        		System.out.println("No encontre archivo, creando uno: "+logFile.getCanonicalPath());
 
-            // Check real path
-            System.out.println(logFile.getCanonicalPath());
-
-            writer = new BufferedWriter(new FileWriter(logFile, true));
-            writer.write(transaction + "\n");
+        		writer = new BufferedWriter(new FileWriter(logFile, true));
+        		writer.write("{[\n"+transaction + "\n]}\n");
+        		writer.close();
+        	}
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                // Check null pointer
-                writer.close();
-            } catch (IOException e) {
-            }
+            
         }
     }
     private void saveDB() {
         System.err.println("Feature not yet implemented");
     }
+    
+    private boolean checkife(String ipath, String transaction) {
+    	try {
+    		Path path = Paths.get(ipath);
+    		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+    		int position = 1;  
+    		lines.add(position, transaction+",");
+    		Files.write(path, lines, StandardCharsets.UTF_8);
+    		System.out.println("Escribiendo a archivo en posición: "+ Integer.toString(position) );
+    	} catch(Exception e) {
+    		System.err.println(e.getMessage());
+    		return false;
+    	} finally {
+    		
+    	}
+    	return true;
+    }
+    public static String readdata(String fileName) {
+        // This will reference one line at a time
+        String line = null;
+        String salida = "";
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                salida += line;
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+        return salida;
+    }
 }
-
-/*
-In Java 7+ you can use the Files and Path class as following:
-
-List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-lines.add(position, extraLine);
-Files.write(path, lines, StandardCharsets.UTF_8);
-To give an example:
-
-Path path = Paths.get("C:\\Users\\foo\\Downloads\\test.txt");
-List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-
-int position = lines.size() / 2;
-String extraLine = "This is an extraline";  
-
-lines.add(position, extraLine);
-Files.write(path, lines, StandardCharsets.UTF_8);
-
-*/
